7cb61d13840859f409d67be4a7d3bfc91e7aff87
Janet <janet.mamani@itscordoba.edu.ar>
2023-08-25 20:50:35 -0300

    se realizo el tp4

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const apikey = "SeT7vfMLkvNrL8IB515A68MBsLG2HFxu";
+const termino = document.querySelector(".termino");
+const buscador = document.querySelector(".buscador");
+const resultados = document.querySelector(".resultados");
+
+buscador.addEventListener("click", () => {
+    resultados.innerHTML = "";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${termino.value}&limit=9`;
+    const peticion = fetch(url);
+    peticion
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            response.data.forEach(element => {
+                const urlImg = element.images.original.url;
+                const img = document.createElement("img");
+                img.src = urlImg;
+                resultados.appendChild(img); 
+                
+            });
+                
+            })
+        })
